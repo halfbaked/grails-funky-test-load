1b9a3cb5dc07829a7d178c3e062d4298eb76eb51
34m0 <eamonnoconnell@gmail.com>
2012-12-03 10:40:21 +0100

    fixed issue preventing plugin from being published correctly

--- FunkyTestLoadGrailsPlugin.groovy
+++ FunkyTestLoadGrailsPlugin.groovy
@@ -1,9 +1,9 @@
 class FunkyTestLoadGrailsPlugin {
-    def version = "0.3.8"
+    def version = "0.3.9"
     def grailsVersion = "2.0 > *"
     def title = "Funky Test Load Plugin"
     def author = "Eamonn O'Connell"
-    def authorEmail = "twitter:@34m0"
+    def authorEmail = "me@34m0.com"
     def description = '''\
 Enables functional tests to be used as light load tests. Once you install this plugin, your application will 
 be able to run functional tests in parallel, provided you have already done the necessary, to setup your 
@@ -14,4 +14,7 @@ application to run functional tests.
     def license = "APACHE"
     def issueManagement = [ system: "Github", url: "https://github.com/halfbaked/grails-funky-test-load/issues" ]
     def scm = [ url: "https://github.com/halfbaked/grails-funky-test-load" ]
+    def developers = [
+      [name: "Eamonn O'Connell", email: "me@34m0.com"]
+    ]
 }
